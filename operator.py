13d4f1e60e7fc7296ca04954e130508c06f1371b
abeez-alam <132378812+abeez-alam@users.noreply.github.com>
2024-08-09 11:19:03 -0700

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,27 @@
+# art operator
+
+
+# a = 2
+
+# b = 48
+
+# c = a + b
+
+# print(c)
+
+
+# assignment operator
+
+# a = 4-2
+# print(a)
+
+# b = 6
+# b += 3
+
+# print(b)
+
+
+a = int(input("enter num 1 :"))
+b = int(input("enter num 2 :"))
+
+print("sum of num is:", a + b)
